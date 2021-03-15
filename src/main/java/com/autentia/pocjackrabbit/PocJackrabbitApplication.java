c6334d59b296dd8453d0eaa9f5a7affe9e9ea7df
Pablo Betancor <pablo.betancor@autentia.com>
2021-03-15 15:55:33 +0100

    first commit

---
+++ src/main/java/com/autentia/pocjackrabbit/PocJackrabbitApplication.java
@@ -0,0 +1,13 @@
+package com.autentia.pocjackrabbit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocJackrabbitApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PocJackrabbitApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/autentia/pocjackrabbit/PocJackrabbitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autentia.pocjackrabbit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocJackrabbitApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
